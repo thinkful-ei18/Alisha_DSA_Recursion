99df71e1fbf2c25e01220c780a78d30a00334456
alishaevn <alieva.inc@gmail.com>
2018-04-03 12:33:57 -0700

    it works!

--- anagrams.js
+++ anagrams.js
@@ -6,47 +6,75 @@ Hint: For your algorithm, you might want to think about a prefix and use that to
  */
 
 const anagrams = string => {
-
-  if(string.length < 1) return console.log(string);
 	
   let words = [];
 
-  for (let i=0; i < string.length; i++) {
-    // console.log(string[i]);
-    console.log(`${string.slice(0, i)}${string.slice(i+1)}`);
-    words.push[`${string[i]}${string.slice(i + 1)}`];
-    // console.log(string.slice(i));
-    // console.log(anagrams(string.slice(i)));
-
-    return [words, ...anagrams()];
+  if (string.length === 2) {
+    const alpha = string[0] + string[1];
+    const beta = string[1] + string[0];
+    return [ alpha, beta ];
   }
 
+  // let firstLetter = string[0]; 
+  // let remainingLetters = anagrams(string.slice(1)); // [as, sa]
+  // words = [...words, ...remainingLetters.map(characters => `${firstLetter}${characters}`)];
+
+  // firstLetter = string[1];
+  // remainingLetters = anagrams(`${string[0]}${string[2]}`); // [es, se]
+  // words = [...words, ...remainingLetters.map(characters => `${firstLetter}${characters}`)];
+
+  // firstLetter = string[2];
+  // remainingLetters = anagrams(`${string[0]}${string[1]}`); // [ea, ae]
+  // words = [...words, ...remainingLetters.map(characters => `${firstLetter}${characters}`)];
+
+
+  for( let i=0; i<string.length; i++ ) {
+    let selectedLetter = string[i];
+    let remainingString = anagrams( removedIndex(string, i) ); // 
+    words = [...words, ...remainingString.map(characters => `${selectedLetter}${characters}`)];
+  }
+
+  return words;
 };
 
-// console.log(anagrams('east'));
-anagrams('east');
+const removedIndex = (string, index) => {
+  // const string = 'alisha'
+  // string.slice(0, 0) + string.slice(1) = 'lisha'
+  // string.slice(0, 1) + string.slice(2) = 'aisha'
+  // string.slice(0, 2) + string.slice(3) = 'alsha'
+  // string.slice(0, 3) + string.slice(4) = 'aliha'
+  // string.slice(0, 4) + string.slice(5) = 'alisa'
+  // string.slice(0, 5) + string.slice(6) = 'alish'
+  const remainingLetters = `${string.slice(0, index)}${string.slice(index+1)}`;
+  return remainingLetters;
+};
+
+
+// console.log(anagrams('ea'));
+// console.log(anagrams('eas'));
+console.log(anagrams('east'));
 
 /**
  * east
  * eats
  * etas
- * esat
  * etsa
+ * esat
  * esta
  * 
  * aest
+ * aets
  * aset
  * aste
- * aets
  * ates
  * atse
  * 
  * seat
+ * seta
  * saet
+ * sate
  * stae
  * stea
- * seta
- * sate
  * 
  * teas
  * tesa
